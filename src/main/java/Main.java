dc682a0ab5ea80ca2deb7fc25ea06b987d9b988d
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-03-10 14:33:49 +0200

    Add missing line break in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -44,7 +44,7 @@ public class Main {
                             System.out.println("\nInvalid date/time format. Returning to main menu.");
                             break;
                         } catch (IllegalArgumentException e) {
-                            System.out.println(e.getMessage());
+                            System.out.println("\n" + e.getMessage());
                         }
                     }
                     break;
